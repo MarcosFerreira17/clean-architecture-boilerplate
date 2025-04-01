41b3d5d28e8189752ceed6b391703577f5e2217f
MarcosFerreira17 <marcosfw7@outlook.com>
2025-04-01 20:42:33 -0300

    feat: add result pattern

---
+++ src/Core/Application/DTO/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.DTO;
+public class Result
+{
+    public bool IsSuccess { get; }
+    public List<string> Errors { get; }
+
+    private Result(bool isSuccess, List<string> errors)
+    {
+        IsSuccess = isSuccess;
+        Errors = errors;
+    }
+
+    public static Result Success() => new Result(true, new List<string>());
+    public static Result Failure(List<string> errors) => new Result(false, errors);
+    public static Result Failure(string error) => new Result(false, new List<string> { error });
+}
---
+++ src/Core/Application/DTO/ResultGeneric.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.DTO;
+public class ResultGeneric<T>
+{
+    public T Value { get; }
+    public bool IsSuccess { get; }
+    public List<string> Errors { get; }
+
+    private ResultGeneric(T value, bool isSuccess, List<string> errors)
+    {
+        Value = value;
+        IsSuccess = isSuccess;
+        Errors = errors;
+    }
+
+    public static ResultGeneric<T> Success(T value) => new ResultGeneric<T>(value, true, new List<string>());
+
+    public static ResultGeneric<T> Failure(List<string> errors) => new ResultGeneric<T>(default, false, errors);
+
+    public static ResultGeneric<T> Failure(string error) => new ResultGeneric<T>(default, false, new List<string> { error });
+}
