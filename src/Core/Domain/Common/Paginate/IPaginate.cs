705e7a1563921253c06e2d5befc21e4d7f666046
MarcosFerreira17 <marcosfw7@outlook.com>
2025-03-31 23:14:00 -0300

    feat: add paginate

---
+++ src/Core/Domain/Common/Paginate/IPaginate.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Common.Paginate;
+
+public interface IPaginate<T>
+{
+    int From { get; }
+
+    int Index { get; }
+
+    int Size { get; }
+
+    int Count { get; }
+
+    int Pages { get; }
+
+    IList<T> Items { get; }
+
+    bool HasPrevious { get; }
+
+    bool HasNext { get; }
+}
---
+++ src/Core/Domain/Common/Paginate/IPaginateExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Common.Paginate;
+
+public static class PaginateExtensions
+{
+    public static IPaginate<T> ToPaginate<T>(this IEnumerable<T> source, int index, int size, int from = 0)
+    {
+        return new Paginate<T>(source, index, size, from);
+    }
+
+    public static IPaginate<TResult> ToPaginate<TSource, TResult>(this IEnumerable<TSource> source,
+        Func<IEnumerable<TSource>, IEnumerable<TResult>> converter, int index, int size, int from = 0)
+    {
+        return new Paginate<TSource, TResult>(source, converter, index, size, from);
+    }
+}
